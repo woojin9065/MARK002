dd40e623b5aace7b29d1bc94b7511ae86a79e08d
woojin9065 <woojin@welgram.com>
2022-03-31 19:16:53 +0900

    04.자바프로그래밍기초 - commiit #04

--- MARK002/src/PART01_BeginnerStep/ch04_자바프로그래밍기초/Hello.java
+++ MARK002/src/PART01_BeginnerStep/ch04_자바프로그래밍기초/Hello.java
@@ -6,6 +6,6 @@ package PART01_BeginnerStep.ch04_자바프로그래밍기초;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World"); 			// ���ڿ� ���
+		System.out.println("Hello World"); 			// 문자열 출력
 	}
 }
