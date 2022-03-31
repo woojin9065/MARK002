780ba9fd4c2b1f160087124e22155fdfe632c28b
woojin9065 <woojin@welgram.com>
2022-03-31 16:22:48 +0900

    04.자바프로그래밍기초 - commiit #01

---
+++ MARK002/src/PART01_BeginnerStep/ch04_자바프로그래밍기초/Hello.java
@@ -0,0 +1,11 @@
+package PART01_BeginnerStep.ch04_자바프로그래밍기초;
+
+/**
+ * 표준 출력으로 "Hello World"를 표시하느 간단한 자바 어플리케이션 구현이다. 
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); 			// 문자열 출력
+	}
+}
